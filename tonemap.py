87ac29be573dae0b37861e850e547525a445a00b
Alyx <akw@en0.io>
2020-07-04 23:01:27 -0700

    added tone map

---
+++ tonemap.py
@@ -0,0 +1,290 @@
+map_a = {
+    "file": "audio/LETTERS/A.wav",
+    "special": False
+}
+
+map_b = {
+    "file": "audio/LETTERS/B.wav",
+    "special": False
+}
+
+map_c = {
+    "file": "audio/LETTERS/C.wav",
+    "special": False
+}
+
+map_d = {
+    "file": "audio/LETTERS/D.wav",
+    "special": False
+}
+
+map_e = {
+    "file": "audio/LETTERS/E.wav",
+    "special": False
+}
+
+map_f = {
+    "file": "audio/LETTERS/F.wav",
+    "special": False
+}
+
+map_g = {
+    "file": "audio/LETTERS/G.wav",
+    "special": False
+}
+
+map_h = {
+    "file": "audio/LETTERS/H.wav",
+    "special": False
+}
+
+map_i = {
+    "file": "audio/LETTERS/I.wav",
+    "special": False
+}
+
+map_j = {
+    "file": "audio/LETTERS/J.wav",
+    "special": False
+}
+
+map_k = {
+    "file": "audio/LETTERS/K.wav",
+    "special": False
+}
+
+map_l = {
+    "file": "audio/LETTERS/L.wav",
+    "special": False
+}
+
+map_m = {
+    "file": "audio/LETTERS/M.wav",
+    "special": False
+}
+
+map_n = {
+    "file": "audio/LETTERS/N.wav",
+    "special": False
+}
+
+map_o = {
+    "file": "audio/LETTERS/O.wav",
+    "special": False
+}
+
+map_p = {
+    "file": "audio/LETTERS/P.wav",
+    "special": False
+}
+
+map_q = {
+    "file": "audio/LETTERS/Q.wav",
+    "special": False
+}
+
+map_r = {
+    "file": "audio/LETTERS/R.wav",
+    "special": False
+}
+
+map_s = {
+    "file": "audio/LETTERS/S.wav",
+    "special": False
+}
+
+map_t = {
+    "file": "audio/LETTERS/T.wav",
+    "special": False
+}
+
+map_u = {
+    "file": "audio/LETTERS/U.wav",
+    "special": False
+}
+
+map_v = {
+    "file": "audio/LETTERS/V.wav",
+    "special": False
+}
+
+map_w = {
+    "file": "audio/LETTERS/W.wav",
+    "special": False
+}
+
+map_x = {
+    "file": "audio/LETTERS/X.wav",
+    "special": False
+}
+
+map_y = {
+    "file": "audio/LETTERS/Y.wav",
+    "special": False
+}
+
+map_z = {
+    "file": "audio/LETTERS/Z.wav",
+    "special": False
+}
+map_1 = {
+    "file": "audio/LETTERS/Q.wav",
+    "special": True
+}
+
+map_2 = {
+    "file": "audio/LETTERS/W.wav",
+    "special": True
+}
+
+map_3 = {
+    "file": "audio/LETTERS/E.wav",
+    "special": True
+}
+
+map_4 = {
+    "file": "audio/LETTERS/R.wav",
+    "special": True
+}
+
+map_5 = {
+    "file": "audio/LETTERS/T.wav",
+    "special": True
+}
+
+map_6 = {
+    "file": "audio/LETTERS/Y.wav",
+    "special": True
+}
+
+map_7 = {
+    "file": "audio/LETTERS/U.wav",
+    "special": True
+}
+
+map_8 = {
+    "file": "audio/LETTERS/I.wav",
+    "special": True
+}
+
+map_9 = {
+    "file": "audio/LETTERS/O.wav",
+    "special": True
+}
+map_0 = {
+    "file": "audio/LETTERS/P.wav",
+    "special": True
+}
+map_hyphen = {
+    "file": "audio/LETTERS/A.wav",
+    "special": True
+}
+map_dollar = {
+    "file": "audio/LETTERS/D.wav",
+    "special": True
+}
+map_exclamation = {
+    "file": "audio/LETTERS/F.wav",
+    "special": True
+}
+map_plus = {
+    "file": "audio/LETTERS/G.wav",
+    "special": True
+}
+map_equals = {
+    "file": "audio/LETTERS/H.wav",
+    "special": True
+}
+map_apostrophe = {
+    "file": "audio/LETTERS/J.wav",
+    "special": True
+}
+map_leftparenthesis = {
+    "file": "audio/LETTERS/K.wav",
+    "special": True
+}
+map_rightparenthesis = {
+    "file": "audio/LETTERS/L.wav",
+    "special": True
+}
+map_quotation = {
+    "file": "audio/LETTERS/Z.wav",
+    "special": True
+}
+map_forwardslash = {
+    "file": "audio/LETTERS/X.wav",
+    "special": True
+}
+map_colon = {
+    "file": "audio/LETTERS/C.wav",
+    "special": True
+}
+map_semicolon = {
+    "file": "audio/LETTERS/V.wav",
+    "special": True
+}
+map_question = {
+    "file": "audio/LETTERS/B.wav",
+    "special": True
+}
+map_comma = {
+    "file": "audio/LETTERS/N.wav",
+    "special": True
+}
+map_fullstop = {
+    "file": "audio/LETTERS/M.wav",
+    "special": True
+}
+
+tonemap = {
+    "Q": map_q,
+    "W": map_w,
+    "E": map_e,
+    "R": map_r,
+    "T": map_t,
+    "Y": map_y,
+    "U": map_u,
+    "I": map_i,
+    "O": map_o,
+    "P": map_p,
+    "A": map_a,
+    "S": map_s,
+    "D": map_d,
+    "F": map_f,
+    "G": map_g,
+    "H": map_h,
+    "J": map_j,
+    "K": map_k,
+    "L": map_l,
+    "Z": map_z,
+    "X": map_x,
+    "C": map_c,
+    "V": map_v,
+    "B": map_b,
+    "N": map_n,
+    "M": map_m,
+    "1": map_1,
+    "2": map_2,
+    "3": map_3,
+    "4": map_4,
+    "5": map_5,
+    "6": map_6,
+    "7": map_7,
+    "8": map_8,
+    "9": map_9,
+    "-": map_hyphen,
+    "$": map_dollar,
+    "!": map_exclamation,
+    "+": map_plus,
+    "=": map_equals,
+    "'": map_apostrophe,
+    "(": map_leftparenthesis,
+    ")": map_rightparenthesis,
+    "\"": map_quotation,
+    "/": map_forwardslash,
+    ":": map_colon,
+    ";": map_semicolon,
+    "?": map_question,
+    ",": map_comma,
+    ".": map_fullstop
+}
